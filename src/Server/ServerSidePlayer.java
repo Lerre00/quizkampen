a10475856360054742df23b9ea296b6b8037fda0
ruckel <97101046+ruckel@users.noreply.github.com>
2022-11-22 19:09:12 +0100

    updated answer logic and client response to result

--- src/Server/ServerSidePlayer.java
+++ src/Server/ServerSidePlayer.java
@@ -7,14 +7,13 @@ import java.io.PrintWriter;
 import java.net.Socket;
 
 
-class ServerSidePlayer extends Thread {
+public class ServerSidePlayer extends Thread {
     String playerNumber;
     ServerSidePlayer opponent;
     Socket socket;
     BufferedReader input;
     PrintWriter output;
     ServerSideGame game;
-
     int pointsPlayer1 = 0;
 
     int pointsPlayer2 = 0;
@@ -73,21 +72,37 @@ class ServerSidePlayer extends Thread {
             output.println("6");
             output.println("8");
 
-            //redundant, same stuff as the lines below, used for getting answer
-            String correctAnswer="6";
+            //button 3. answer is not actually 3
+            String correctAnswer="3";
 
-            //Recieve answer from player
-            String playerNumberAndAnswerFromClient = input.readLine();
+            // Repeatedly get commands from the client and process them.
+            while (true) {
+                //Recieve answer from player
+                String playerNumberAndAnswerFromClient = input.readLine();
 
-            //Look if answer was correct and give point if true
-            String playerNumberFromClient = playerNumberAndAnswerFromClient.substring(0,7);
-            String answerFromClient = playerNumberAndAnswerFromClient.substring(7);
-            if (playerNumberFromClient == "player1" && answerFromClient == "Alternativ 3"){
-                setPointsPlayer1(getPointsPlayer1() + 1);
+                //Look if answer was correct and give point if true
+                String playerNumberFromClient = playerNumberAndAnswerFromClient.substring(0, 7);
+                String answerFromClient = playerNumberAndAnswerFromClient.substring(7);
+                if (playerNumberFromClient.equals("player1") && answerFromClient.equals(correctAnswer)) {
+                    setPointsPlayer1(getPointsPlayer1() + 1);
+                    output.println("CORRECT_ANSWER");
+                }
+                if (playerNumberFromClient.equals("player1") && !answerFromClient.equals(correctAnswer)) {
+                    output.println("INCORRECT_ANSWER");
+                }
+                if (playerNumberFromClient.equals("player2") && answerFromClient.equals(correctAnswer)) {
+                    setPointsPlayer2(getPointsPlayer2() + 1);
+                    output.println("CORRECT_ANSWER");
+                }
+                if (playerNumberFromClient.equals("player2") && !answerFromClient.equals(correctAnswer)) {
+                    output.println("INCORRECT_ANSWER");
+                }
+
+                System.out.println(playerNumberFromClient + " answer: " + answerFromClient);
+
             }
-            if (playerNumberFromClient == "player2" && answerFromClient == "Alternativ 3"){
-              setPointsPlayer2(getPointsPlayer2() + 1);
-            }
+
+
 
 
             //Send next question
@@ -96,13 +111,13 @@ class ServerSidePlayer extends Thread {
 
 
             // Repeatedly get commands from the client and process them.
-            while (true) {
-                String right = input.readLine().substring(7);
-                System.out.println(input.readLine().substring(7));
-                if (right.equals(correctAnswer)) {
-                    output.println("CORRECT_ANSWER");
-                }
-            }
+//            while (true) {
+//                String right = input.readLine().substring(7);
+//                System.out.println(input.readLine().substring(7) + "test");
+//                if (answerFromClient.equals(correctAnswer)) {
+//                    output.println("CORRECT_ANSWER");
+//                }
+//            }
         }catch(IOException e){
             e.printStackTrace();
         }
