d7f9bc7c9c13dc8cf2fd2556cce52a59fd104891
Leroy Collazo <leroycollazo@hotmail.com>
2022-11-23 14:18:00 +0100

    first commit of graphics

--- src/Client/QuizkampenClient.java
+++ src/Client/QuizkampenClient.java
@@ -17,16 +17,30 @@ public class QuizkampenClient extends JFrame implements ActionListener {
     JPanel basePanel = new JPanel();
     JPanel southPanel = new JPanel();
     JPanel northPanel = new JPanel();
+
     JPanel northNorthPanel = new JPanel();
     JPanel southNorthPanel = new JPanel();
-    JLabel questionFrame = new JLabel("Waiting for opponent..");
+    JPanel centralNorthPanel = new JPanel();
+
+    JPanel westNorthNorthPanel = new JPanel();
+    JPanel eastNorthNorthPanel = new JPanel();
+    JPanel centralNorthNorthPanel = new JPanel();
+
     JRadioButton radioButton1 = new JRadioButton();
     JRadioButton radioButton2 = new JRadioButton();
     JRadioButton radioButton3 = new JRadioButton();
+
+    JRadioButton opponentRadioButton1 = new JRadioButton();
+    JRadioButton opponentRadioButton2 = new JRadioButton();
+    JRadioButton opponentRadioButton3 = new JRadioButton();
+
     JButton button1 = new JButton();
     JButton button2 = new JButton();
     JButton button3 = new JButton();
     JButton button4 = new JButton();
+
+    JLabel themeLabel = new JLabel("Tema: Natur");
+    JLabel questionFrame = new JLabel("Waiting for opponent...");
     JProgressBar progressBar = new JProgressBar();
 
 
@@ -44,26 +58,47 @@ public class QuizkampenClient extends JFrame implements ActionListener {
         out = new PrintWriter(socket.getOutputStream(), true);
 
         questionFrame.setFont(new Font("Arial", Font.BOLD, 20));
+
         basePanel.setLayout(new BorderLayout());
         northPanel.setLayout(new BorderLayout());
+        northNorthPanel.setLayout(new BorderLayout());
+        eastNorthNorthPanel.setLayout(new GridLayout(3, 1));
+        westNorthNorthPanel.setLayout(new GridLayout(3,1));
         southPanel.setLayout(new GridLayout(2,2));
-        northPanel.setPreferredSize(new Dimension(100, 100));
-        southPanel.setPreferredSize(new Dimension(150, 150));
+
+        northPanel.setPreferredSize(new Dimension(100, 150));
+        southPanel.setPreferredSize(new Dimension(150, 100));
+
         button1.addActionListener(this);
-        button2.addActionListener( this);
-        button3.addActionListener( this);
-        button4.addActionListener( this);
+        button2.addActionListener(this);
+        button3.addActionListener(this);
+        button4.addActionListener(this);
         southPanel.add(button1);
         southPanel.add(button2);
         southPanel.add(button3);
         southPanel.add(button4);
+
+        westNorthNorthPanel.add(radioButton1);
+        westNorthNorthPanel.add(radioButton2);
+        westNorthNorthPanel.add(radioButton3);
+
+        eastNorthNorthPanel.add(opponentRadioButton1);
+        eastNorthNorthPanel.add(opponentRadioButton2);
+        eastNorthNorthPanel.add(opponentRadioButton3);
+
+        centralNorthNorthPanel.add(themeLabel);
+
+        northNorthPanel.add(westNorthNorthPanel, BorderLayout.WEST);
+        northNorthPanel.add(eastNorthNorthPanel, BorderLayout.EAST);
+        northNorthPanel.add(centralNorthNorthPanel, BorderLayout.CENTER);
+
         northPanel.add(northNorthPanel, BorderLayout.NORTH);
+        northPanel.add(centralNorthPanel, BorderLayout.CENTER);
         northPanel.add(southNorthPanel, BorderLayout.SOUTH);
+
+        centralNorthPanel.add(questionFrame, BorderLayout.CENTER);
         southNorthPanel.add(progressBar, BorderLayout.CENTER);
-        northNorthPanel.add(radioButton1);
-        northNorthPanel.add(radioButton2);
-        northNorthPanel.add(radioButton3);
-        northPanel.add(questionFrame);
+
         basePanel.add(northPanel, BorderLayout.NORTH);
         basePanel.add(southPanel);
         add(basePanel);
@@ -112,7 +147,7 @@ public class QuizkampenClient extends JFrame implements ActionListener {
         while (true) {
             String serverAddress = (args.length == 0) ? "localhost" : args[1];
             QuizkampenClient client = new QuizkampenClient(serverAddress);
-            client.setSize(350,350);
+            client.setSize(350,300);
             client.setLocation(100,100);
             client.setVisible(true);
             client.setDefaultCloseOperation(EXIT_ON_CLOSE);
